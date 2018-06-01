ae2210913163a6ba80f72ee072cf788319ded9f1
DESKTOP-B9HHJSF\Davide <davide.bonafede9@gmail.com>
2018-06-02 01:00:46 +0200

    create an image instead of pdf. Missing auto resize of the movie ratio

--- redux.py
+++ redux.py
@@ -1,34 +1,41 @@
+# Cinema Redux 			
+# Created by Davide Bonafede	
 import cv2
 import time
-from fpdf import FPDF
-vidcap = cv2.VideoCapture('Il Viale Del Tramonto.mp4')
+import numpy as np
+#Video file importing
+vidcap = cv2.VideoCapture('Happy Family.mkv')
 count = 0
 success = True
-pdf = FPDF('P', 'mm', 'A4')
-pdf.add_page()
-x = 0
-y = 0
+firstLine = 1
+# width and heigth of resized frames
 w = 80
 h = 45
 z = 0
-nFrames = 1500
-sFactor = 16
+nFrameforLine = 60
+oneFrameEvery = 24
+success,image = vidcap.read()
 while success:
-	if(count % 24 == 0):
-		image = cv2.resize(image, (w, h))
-		cv2.imwrite("frame%d.jpg" % z, image)
-		z += 1
+	if(count % oneFrameEvery == 0):
+		image = cv2.resize(image, (w, h))	# image resize
+		if z == 0:							# first image of the line 
+			imageLine = image
+			z += 1
+		else:
+			imageLine = np.concatenate((imageLine, image), axis=1)
+			z += 1
+			if z == nFrameforLine:			# the line is finished
+				if firstLine:
+					Image = imageLine
+					firstLine = 0
+					z = 0
+				else:
+					Image = np.concatenate((Image, imageLine), axis=0)
+					z = 0
 	success,image = vidcap.read()
-	print ('Read a new frame: ', success)
-	if(z == nFrames): 
-		break
 	count += 1
-z = 0
-for g in range(nFrames):
-	if(g % 42 == 0 and g != 0): 
-		x = 0 
-		y += h
-	pdf.image('frame' + str(g) + '.jpg', x / sFactor, y / sFactor, w / sFactor, h / sFactor, 'JPG', '')
-	z += 1
-	x +=  w
-pdf.output('es.pdf', 'F')
+# fill with black missing frames to complete image
+for x in range(nFrameforLine - z):
+	imageLine = np.concatenate((imageLine, np.zeros((h,w,3), np.uint8)), axis=1)
+Image = np.concatenate((Image, imageLine), axis=0)
+cv2.imwrite("redux.jpg", Image)
